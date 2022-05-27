5c664fb48fca40368f3609075555b352d41ee5d6
Shun Sugawara <82685781shun-syun-611@users.noreply.github.com>
2022-05-27 21:02:58 +0900

    render html

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # python_todo_app
+
+$ pip install flask flask-sqlalchemy
+
+$ pip install --upgrade pip
+
+# Mac の場合
+
+$ source env/bin/activate
+
+# Windows の場合
+
+$ env\Scripts\activate
--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World"
+    # ここでindex.htmlを読み込んで表示
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
