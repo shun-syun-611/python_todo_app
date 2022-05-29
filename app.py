31036a8a9a0ef264de07c08a83cb48079c60f1dc
Shun Sugawara <82685781shun-syun-611@users.noreply.github.com>
2022-05-29 16:08:02 +0900

    create db and list display

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    detail = db.Column(db.String(100))
+    due = db.Column(db.DateTime, nullable=False)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    # ここでindex.htmlを読み込んで表示
-    return render_template('index.html')
+    if request.method == "GET":
+        posts = Post.query.all()
+        return render_template('index.html', posts=posts)
+    
+    if request.method == "POST":
+        title = request.form.get("title")
+        detail = request.form.get("detail")
+        due = request.form.get("due")
+        
+        due = datetime.strptime(due, '%Y-%m-%d')
+        new_post = Post(title=title, detail=detail, due=due)
+        
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/')
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
